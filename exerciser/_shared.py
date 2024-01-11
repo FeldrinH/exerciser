19f92cbb907e7daa0cb99509914b563c652ab70d
Juhan Oskar Hennoste <juhanoskar@gmail.com>
2024-01-11 23:43:12 +0200

    Update comment

--- exerciser/_shared.py
+++ exerciser/_shared.py
@@ -8,8 +8,7 @@ class ValidationError(RuntimeError):
 class CodeRunError(RuntimeError):
     pass
 
-# Note: Even though Exercise is structurally typed, it is recommended to explicitly subclass Exercise
-# for better type hints and default implementations of methods.
+# Note: Even though Exercise is structurally typed, it is recommended to explicitly subclass Exercise for better type hints.
 class Exercise(Protocol):
     name: str
 
