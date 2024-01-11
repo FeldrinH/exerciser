28ec8fae74127400d1e44cc059b5ba465d2e8798
Juhan Oskar Hennoste <juhanoskar@gmail.com>
2024-01-11 19:35:22 +0200

    Make arrows smaller

--- exerciser/pygame.py
+++ exerciser/pygame.py
@@ -9,7 +9,7 @@ ColorValue = Union[pygame.Color, int, str, Tuple[int, int, int], RGBAOutput, Seq
 def draw_arrow(surface: pygame.Surface, color: ColorValue, start_pos: Coordinate, offset: Coordinate, width: int):
     end_pos_vec = pygame.Vector2(start_pos[0] + offset[0], start_pos[1] + offset[1])
     dir_vec = pygame.Vector2(offset[0], offset[1])
-    length = min(10, dir_vec.length() * 0.9)
+    length = min(8, dir_vec.length())
     if dir_vec.length_squared() > 0:
         dir_vec.normalize_ip()
     pygame.draw.line(surface, color, start_pos, end_pos_vec, width)
