fc79c6e55a5c046b85413bcca06674cb8a1d498d
Juhan Oskar Hennoste <juhanoskar@gmail.com>
2024-02-27 17:22:59 +0200

    Add draw_figure

--- exerciser/pygame.py
+++ exerciser/pygame.py
@@ -1,5 +1,6 @@
 from typing import Sequence, Tuple, Union
 import pygame
+from matplotlib.backends.backend_agg import FigureCanvasAgg
 
 # Types copied from pygame/_common.pyi
 Coordinate = Union[Tuple[float, float], Sequence[float], pygame.Vector2]
@@ -57,3 +58,18 @@ def draw_spring(surface: pygame.Surface, color: ColorValue, start: Coordinate, e
     pygame.draw.line(surface, color, pos, pos + 0.5 * coil_line, width)
     pos += 0.5 * coil_line
     pygame.draw.line(surface, color, pos, end, width)
+
+def draw_figure(surface: pygame.Surface, canvas: FigureCanvasAgg, left: float, top: float, width: float, height: float):
+    """Draws the Matplotlib figure attached to the given canvas"""
+    width, height = int(width), int(height)
+    size = canvas.get_width_height(physical=True)
+    if width != size[0] or height != size[1]:
+        # Note: No layout engine (the default) generally does not work well when resizing figures, so we automatically apply tight layout.
+        # Keeping tight layout (or any layout engine) constantly enabled is not desireable, because it hurts drawing performance.
+        dpi = canvas.figure.get_dpi()
+        canvas.figure.set_size_inches((width + 0.25) / dpi, (height + 0.25) / dpi, forward=False)
+        canvas.figure.tight_layout()
+        size = canvas.get_width_height(physical=True)
+    canvas.draw()
+    image = pygame.image.frombuffer(canvas.buffer_rgba(), size, 'RGBA')
+    surface.blit(image, (left, top))
